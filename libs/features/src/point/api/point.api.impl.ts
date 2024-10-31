8e95fb0209b6a45bfcf3c0180f3eebd79a4bf650
jys9962 <jys9962@kakao.com>
2024-10-31 14:41:09 +0900

    포인트 도메인

--- libs/features/src/point/api/point.api.impl.ts
+++ libs/features/src/point/api/point.api.impl.ts
@@ -4,8 +4,6 @@ import { PointLog } from '@libs/features/point/domain/point-log';
 import { Result, ResultSync } from '@libs/common/model/result/result';
 import { RefundPointError, UsePointError } from '@libs/features/point/domain/point.error';
 import { PointLogRepository } from '@libs/features/point/domain/point-log.repository';
-import { PointAccount } from '@libs/features/point/domain/point-account';
-import { Timestamp } from '@libs/common/types/timestamp';
 
 @Injectable()
 export class PointApiImpl implements PointApi {
@@ -18,131 +16,36 @@ export class PointApiImpl implements PointApi {
     userId: string,
     point: number,
     expiredAt?: Date,
-    memo: string = '',
+    memo?: string,
   ): Promise<void> {
-    await this.addToLog(userId, {
-      type: 'ADD',
-      amount: point,
-      memo: memo,
-      expiredAt: expiredAt || null,
-      createdAt: new Date(),
-    });
+    return Promise.resolve(undefined);
   }
 
-  async get(
-    userId: string,
-  ): Promise<number> {
-    const account = await this.restoreAccount(userId);
-
-    return account.get();
+  async get(userId: string): Promise<number> {
+    return Promise.resolve(0);
   }
 
-  async use(
-    userId: string,
-    point: number,
-    transactionId: string,
-    memo: string = '',
-  ): ResultSync<void, UsePointError> {
-    const current = Timestamp.now();
-    const account = await this.restoreAccount(userId);
-    const result = account.use({
-      amount: point,
-      transactionId: transactionId,
-      executedAt: current,
-    });
-
-    if (result.isError) {
-      return result;
-    }
-
-    await this.addToLog(userId, {
-      type: 'USE',
-      amount: point,
-      transactionId: transactionId,
-      createdAt: new Date(current),
-      memo: memo,
-    });
-
-    return Result.ok();
-  }
-
-  async getHistories(
-    userId: string,
-  ): Promise<PointLog[]> {
-    return this.pointLogRepository.get(userId);
+  async getHistories(userId: string): Promise<PointLog[]> {
+    return Promise.resolve([]);
   }
 
   async refund(
     userId: string,
     point: number,
     transactionId: string,
-    memo: string = '',
+    memo?: string,
   ): ResultSync<void, RefundPointError> {
-    const current = Timestamp.now();
-    const account = await this.restoreAccount(userId);
-
-    const result = account.refund({
-      amount: point,
-      transactionId: transactionId,
-      executedAt: current,
-    });
-
-    if (result.isError) {
-      return result;
-    }
-
-    await this.addToLog(userId, {
-      type: 'REFUND',
-      amount: point,
-      transactionId: transactionId,
-      memo: memo,
-      createdAt: new Date(current),
-    });
-
     return Result.ok();
   }
 
-  private async restoreAccount(userId: string) {
-    const list = await this.pointLogRepository.get(userId);
-    list.sort((
-      t1,
-      t2,
-    ) => +t1.createdAt - +t2.createdAt);
-
-    const account = PointAccount.create();
-    for (const log of list) {
-      switch (log.type) {
-        case 'ADD':
-          account.add({
-            amount: log.amount,
-            expiredAt: log.expiredAt ? Timestamp.of(log.expiredAt) : undefined,
-            executedAt: Timestamp.of(log.createdAt),
-          });
-          break;
-        case 'USE':
-          account.use({
-            amount: log.amount,
-            transactionId: log.transactionId || undefined,
-            executedAt: Timestamp.of(log.createdAt),
-          });
-          break;
-        case 'REFUND':
-          account.refund({
-            amount: log.amount,
-            transactionId: log.transactionId,
-            executedAt: Timestamp.of(log.createdAt),
-          });
-          break;
-      }
-    }
-
-    return account;
-  }
-
-  private async addToLog(
+  async use(
     userId: string,
-    logs: PointLog | PointLog[],
-  ) {
-    await this.pointLogRepository.add(userId, logs);
+    point: number,
+    transactionId: string,
+    memo?: string,
+  ): ResultSync<void, UsePointError> {
+    return Result.ok();
   }
+
+
 }
